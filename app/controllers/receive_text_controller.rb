d9f7fe399e52550a36bb5fda4ade2da02705ac5d
Nathan Owsiany <nowsiany@gmail.com>
2015-03-04 21:05:11 -0700

    retry reply message

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -3,9 +3,10 @@ class ReceiveTextController < ApplicationController
     # let's pretend that we've mapped this action to
     # http://localhost:3000/sms in the routes.rb file
 
-    @message_body = params["Body"]
-    @from_number = params["From"]
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Hey Monkey. Thanks for the message!"
+    end
 
-    render 'process_sms.xml.erb', content_type: 'text/xml'
+    render plain: twiml.text
   end
 end
