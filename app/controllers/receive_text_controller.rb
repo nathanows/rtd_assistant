ae9b6e06fb6eb23c6ee867f947098b8447d62874
Nathan Owsiany <nowsiany@gmail.com>
2015-03-04 21:11:14 -0700

    updated response

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -3,10 +3,10 @@ class ReceiveTextController < ApplicationController
     # let's pretend that we've mapped this action to
     # http://localhost:3000/sms in the routes.rb file
 
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Hey Monkey. Thanks for the message!"
-    end
+    twiml = "<Response>
+      <Sms>Thanks for your message!</Sms>
+    </Response>"
 
-    render plain: twiml.text
+    render plain: twiml
   end
 end
